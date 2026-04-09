5c0b01e109c5ce050d711f97606bba21b0b30687
Sourav Samanta <sourav.samanta@nagarro.com>
2026-04-09 20:56:31 +0530

    Resolving all issues

--- CucumberMavenProject/src/main/java/reusableComponents/PropertiesOperation.java
+++ CucumberMavenProject/src/main/java/reusableComponents/PropertiesOperation.java
@@ -7,31 +7,30 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesOperation {
-	public static Properties prop = null;
-	public static File file = null;
-	public static FileInputStream fis = null;
-	static
-	{
-	file = new File(System.getProperty("user.dir") + "\\config.properties");
-	try {
-	fis = new FileInputStream(file);
-	} catch (FileNotFoundException e1) {
-	e1.printStackTrace();
-	}
-	prop = new Properties();
-	try {
-	prop.load(fis);
-	} catch (IOException e) {
-	e.printStackTrace();
-	}
-	}
-	
-	
-	public  static String getPropertyValue(String property)
-	{
-			
-			String value=prop.get(property).toString();
-			return value;		
-			}
-	
-	}
+    public static Properties prop = null;
+    public static File file = null;
+    public static FileInputStream fis = null;
+
+    static {
+        file = new File(System.getProperty("user.dir") + "\\config.properties");
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        prop = new Properties();
+        try {
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static String getPropertyValue(String property) {
+
+        String value = prop.get(property).toString();
+        return value;
+    }
+
+}
